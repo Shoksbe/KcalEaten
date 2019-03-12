86d644815efbb169a2c1812ce522e2a28dae209d
Greg De Knyf <deknyfgregory@gmail.com>
2019-03-12 21:55:00 +0100

    add api call with openFoodFact to get product's info from barcode

---
+++ KcalEaten/Model/Services/OpenFoodFactDecodable.swift
@@ -0,0 +1,43 @@
+//
+//  OpenFoodFactDecodable
+//  KcalEaten
+//
+//  Created by Gregory De knyf on 12/03/2019.
+//  Copyright © 2019 Gregory De knyf. All rights reserved.
+//
+
+import Foundation
+
+struct OpenFoodFactDecodable: Decodable {
+    let product: Product?
+    let statutsVerbose: String
+
+    enum CodingKeys: String, CodingKey {
+        case product
+        case statutsVerbose = "status_verbose"
+    }
+}
+
+struct Product: Decodable {
+    let imageURL: String?
+    let name: String
+    let barCode: String
+    let nutriments: Nutriments
+
+    enum CodingKeys: String, CodingKey {
+        case imageURL = "image_url"
+        case name = "product_name_fr"
+        case barCode = "code"
+        case nutriments
+    }
+}
+
+struct Nutriments: Decodable {
+    let energy, energyUnit, energy100G: String
+
+    enum CodingKeys: String, CodingKey {
+        case energy
+        case energyUnit = "energy_unit"
+        case energy100G = "energy_100g"
+    }
+}
---
+++ KcalEaten/Model/Services/OpenFoodFactService.swift
@@ -0,0 +1,82 @@
+//
+//  OpenFoodFactService.swift
+//  KcalEaten
+//
+//  Created by Gregory De knyf on 12/03/2019.
+//  Copyright © 2019 Gregory De knyf. All rights reserved.
+//
+
+import Foundation
+
+class OpenFoodFactService {
+
+    private var _task: URLSessionTask?
+    private var _session = URLSession(configuration: .default)
+
+    init(session: URLSession) {
+        self._session = session
+    }
+
+    func getProduct(from barCode: String, callback: @escaping (Bool, ProductObject?, String?) -> Void) {
+print("test")
+
+        let urlString = "https://fr.openfoodfacts.org/api/v0/produit/\(barCode).json"
+
+        guard let url = URL(string: urlString) else {
+            callback(false, nil, "OpenFoodFactService: Failed to create Url")
+            return
+        }
+
+        _task?.cancel()
+        _task = _session.dataTask(with: url) { (data, response, error) in
+            DispatchQueue.main.async {
+            //Check if data, no error and httpResponseCode is ok
+            guard let data = data, error == nil,
+                let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                    callback(false, nil, "OpenFoodFactService: No data")
+                    return
+            }
+
+            //Decode jsonFiles
+            guard let responseJSON = try? JSONDecoder().decode(OpenFoodFactDecodable.self, from: data) else {
+                callback(false, nil, "OpenFoodFactService: Failed to decode")
+                return
+            }
+
+            callback(true, self.parseData(from: responseJSON), nil)
+            }
+        }
+        _task?.resume()
+    }
+
+    private func parseData(from data: OpenFoodFactDecodable) -> ProductObject? {
+
+        guard data.statutsVerbose == "product found", let product = data.product else {
+            return nil
+        }
+
+        //Get image data
+        var imageData: Data?
+        if let imageUrlString = product.imageURL {
+            if let imageUrl = URL(string: imageUrlString) {
+                imageData = try? Data(contentsOf: imageUrl)
+            }
+        }
+
+        //Get kcalorie from kj
+        var kcalByGrams = 0.0
+        if let energyDouble = Double(product.nutriments.energy100G) {
+            kcalByGrams = (energyDouble / 100.0) * 0.23
+        }
+
+
+        let productObject = ProductObject(context: AppDelegate.viewContext)
+        productObject.barCode = product.barCode
+        productObject.date = Date()
+        productObject.imageData = imageData
+        productObject.kCalByGrams = Int32(kcalByGrams)
+        productObject.name = product.name
+
+        return productObject
+    }
+}
