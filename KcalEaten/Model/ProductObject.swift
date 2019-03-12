ebec0cf3d89717c571bb5ae31389c717beb5b65b
Greg De Knyf <deknyfgregory@gmail.com>
2019-03-12 21:51:14 +0100

    add coreData implementation

---
+++ KcalEaten/Model/ProductObject.swift
@@ -0,0 +1,14 @@
+//
+//  Product.swift
+//  KcalEaten
+//
+//  Created by Gregory De knyf on 12/03/2019.
+//  Copyright © 2019 Gregory De knyf. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class ProductObject: NSManagedObject {
+
+}
