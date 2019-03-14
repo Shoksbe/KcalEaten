fb9bf25fb0fcc1f95642665e36ae55c3afe6fb9d
Greg De Knyf <deknyfgregory@gmail.com>
2019-03-14 23:19:16 +0100

    fix commit bug

---
+++ KcalEaten/Controller/CameraController.swift
@@ -0,0 +1,16 @@
+//
+//  CameraController.swift
+//  KcalEaten
+//
+//  Created by Gregory De knyf on 12/03/2019.
+//  Copyright © 2019 Gregory De knyf. All rights reserved.
+//
+
+import UIKit
+
+class CameraController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
