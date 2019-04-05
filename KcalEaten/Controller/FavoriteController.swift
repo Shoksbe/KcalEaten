89468dc10898b077cfaf076ec8eb7db55e1323fb
Shoksbe <deknyfgregory@gmail.com>
2019-04-05 12:58:30 +0200

    two-part separation of listOfProductcontroller

--- KcalEaten/Controller/FavoriteController.swift
+++ KcalEaten/Controller/FavoriteController.swift
@@ -12,12 +12,11 @@ import UIKit
 //MARK: - Variables & Outlets
 //---------------------------
 class FavoriteController: UIViewController {
-    private let _showListOfFavoriteSegueId = "showListOfFavorite"
     private let _coreDataService = CoreDataHelper()
-    ///is used to reload the tableview on ListOfProductController
-    private var _containerView: ListOfProductController?
+    private let _productCellId = "productCell"
+    private var _products: [ProductObject]?
 
-    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var tableview: UITableView!
     @IBOutlet weak var titleView: UIView!
 }
 
@@ -27,6 +26,8 @@ class FavoriteController: UIViewController {
 extension FavoriteController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableview.delegate = self
+        tableview.dataSource = self
         NotificationCenter.default.addObserver(self, selector: #selector(updateFavoriteTableview), name: .favoriteStateOfProductDidChange, object: nil)
     }
 
@@ -39,24 +40,85 @@ extension FavoriteController {
 //MARK: - Methods
 //------------------------
 extension FavoriteController {
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        guard segue.identifier == _showListOfFavoriteSegueId else { return }
-        guard let destination = segue.destination as? ListOfProductController else { return }
-        guard let product = try? _coreDataService.fetchFavorite() else { return }
-        destination.product = product.isEmpty ? nil : product
 
-        _containerView = destination
-    }
-    
-    
     /// Get new favorite from database and reload tableview
     @objc func updateFavoriteTableview() {
         guard let products = try? _coreDataService.fetchFavorite(), !products.isEmpty else {
-            _containerView?.reloadTableview(with: nil)
             titleView.isHidden = true
             return
         }
+        self._products = products
+        tableview.reloadData()
         titleView.isHidden = false
-        _containerView?.reloadTableview(with: products)
     }
+}
+
+//-----------------------------------------
+//MARK: - Collection dataSource & Delegate
+//-----------------------------------------
+
+extension FavoriteController: UITableViewDataSource, UITableViewDelegate {
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let count = _products?.count, count > 0 {
+            return count
+        }
+        tableView.isScrollEnabled = false
+        return 0
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //Create cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: _productCellId, for: indexPath) as! ProductCell
+
+        //Get product a index
+        if let product = _products?[indexPath.row] {
+            cell.setupProductWithConsommation(product: product)
+        }
+
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let product = self._products?[indexPath.row] {
+            SHOW_PRODUCT_PAGE(product: product, controller: self)
+        }
+    }
+
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        //Setup container
+        let container = UIView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height))
+
+        //Setup imageView
+        let imageViewWidthAndHeigth: CGFloat = container.frame.width * 0.5
+        let imageView = UIImageView(frame: CGRect(x: imageViewWidthAndHeigth / 2, y: (container.frame.height / 2) - imageViewWidthAndHeigth, width: imageViewWidthAndHeigth, height: imageViewWidthAndHeigth))
+        imageView.contentMode = .scaleAspectFit
+        imageView.image = #imageLiteral(resourceName: "NoFavorite")
+
+        //Setup Label
+        let topMarginToImageView: CGFloat = 10
+        let label = UILabel(frame: CGRect(x: imageView.frame.minX, y: imageView.frame.maxY + topMarginToImageView, width: imageViewWidthAndHeigth, height: 0))
+        label.text = "Vous n'avez encore aucun favoris, scannez un produit afin de pouvoir ajouter un favoris."
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        label.sizeToFit()
+
+        //Add subview
+        container.addSubview(imageView)
+        container.addSubview(label)
+
+        return container
+    }
+
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        if _products == nil {
+            return view.bounds.height
+        }
+        return 0
+    }
+
 }
--- KcalEaten/Controller/ListOfProductController.swift
+++ KcalEaten/Controller/ListOfProductController.swift
@@ -14,36 +14,18 @@ import UIKit
 class ListOfProductController: UIViewController {
     @IBOutlet weak var tableview: UITableView!
     private let _productCellId = "productCell"
-    var product: [ProductObject]?
     var productWithQuantity: [(product:ProductObject, quantity:Int)]?
 }
 
-//------------------
-//MARK: - Life Cycle
-//------------------
-extension ListOfProductController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-}
 //-----------------------------------------
 //MARK: - Collection dataSource & Delegate
 //-----------------------------------------
-
 extension ListOfProductController: UITableViewDataSource, UITableViewDelegate {
 
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if let count = product?.count, count > 0 {
-            return count
-        } else if let count = productWithQuantity?.count,  count > 0 {
+        if let count = productWithQuantity?.count,  count > 0 {
             return count
         }
-        
-        tableView.isScrollEnabled = false
         return 0
     }
 
@@ -54,64 +36,9 @@ extension ListOfProductController: UITableViewDataSource, UITableViewDelegate {
         //Get product a index
         if let product = productWithQuantity?[indexPath.row] {
             cell.setupProductWithConsommation(product: product.product, quantityConsumed: product.quantity)
-        } else if let product = product?[indexPath.row] {
-            cell.setupProductWithConsommation(product: product)
         }
-
 
         return cell
     }
 
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if let product = self.product?[indexPath.row] {
-            SHOW_PRODUCT_PAGE(product: product, controller: self)
-        }
-    }
-
-    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
-        //Setup container
-        let container = UIView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height))
-
-        //Setup imageView
-        let imageViewWidthAndHeigth: CGFloat = container.frame.width * 0.5
-        let imageView = UIImageView(frame: CGRect(x: imageViewWidthAndHeigth / 2, y: (container.frame.height / 2) - imageViewWidthAndHeigth, width: imageViewWidthAndHeigth, height: imageViewWidthAndHeigth))
-        imageView.contentMode = .scaleAspectFit
-        imageView.image = #imageLiteral(resourceName: "NoFavorite")
-
-        //Setup Label
-        let topMarginToImageView: CGFloat = 10
-        let label = UILabel(frame: CGRect(x: imageView.frame.minX, y: imageView.frame.maxY + topMarginToImageView, width: imageViewWidthAndHeigth, height: 0))
-        label.text = "Vous n'avez encore aucun favoris, scannez un produit afin de pouvoir ajouter un favoris."
-        label.textAlignment = .center
-        label.numberOfLines = 0
-        label.sizeToFit()
-
-        //Add subview
-        container.addSubview(imageView)
-        container.addSubview(label)
-
-        return container
-    }
-
-    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
-        if product == nil && productWithQuantity == nil {
-            return view.bounds.height
-        }
-        return 0
-    }
-
-}
-
-//------------------------
-//MARK: - Methods
-//------------------------
-extension ListOfProductController {
-    
-    /// reload tableView
-    ///
-    /// - Parameter products: Products to load
-    func reloadTableview(with products: [ProductObject]?) {
-        product = products
-        tableview.reloadData()
-    }
 }
